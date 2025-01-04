a515482067c5e0aebf2d3190b2517ab1ff10147b
Akshith KL <160204272+akshithkl@users.noreply.github.com>
2025-01-04 22:31:59 +0530

    Create Palindrome_Number.py

---
+++ Easy/Palindrome_Number.py
@@ -0,0 +1,21 @@
+ 
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+
+        if x < 0:
+            return False
+        revers_num = 0
+        y = x
+
+        while x > 0:
+            reminder = x % 10
+            revers_num = revers_num * 10 + reminder
+            x = x // 10
+
+        if y == revers_num:
+            return True
+        else:
+            return False
+        
+        # Time complexity = O(n)
+        # Space complexity = O(1)
