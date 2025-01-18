de074baa5893899518603c089ebce9119f4fa59f
Akshith KL <160204272+akshithkl@users.noreply.github.com>
2025-01-18 16:03:25 +0530

    Update 406_Queue_Reconstruction_by_Height.py

--- Medium/406_Queue_Reconstruction_by_Height.py
+++ Medium/406_Queue_Reconstruction_by_Height.py
@@ -8,6 +8,8 @@ class Solution:
 '''Initial List of Tuples:
 [(-7, 0), (-4, 4), (-7, 1), (-5, 0), (-6, 1), (-5, 2)]
 
+
+
 Compare (-7, 0) and (-4, 4):
 
 First elements: -7 < -4, so (-7, 0) comes before (-4, 4).
@@ -26,6 +28,9 @@ First elements: -5 == -5, so compare second elements: 0 < 2, so (-5, 0) comes be
 Compare (-6, 1) and (-4, 4):
 
 First elements: -6 < -4, so (-6, 1) comes before (-4, 4).
+
+
+
 Fully Sorted List of Tuples:
 After sorting based on the rules: [(-7, 0), (-7, 1), (-6, 1), (-5, 0), (-5, 2), (-4, 4)]
 
