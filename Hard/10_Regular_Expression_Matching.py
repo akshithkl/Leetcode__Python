fa179eec272b0f8d654e3e5e8f39c01a18ba883d
Akshith KL <160204272+akshithkl@users.noreply.github.com>
2025-03-27 09:51:04 +0530

    Update 10_Regular_Expression_Matching.py

--- Hard/10_Regular_Expression_Matching.py
+++ Hard/10_Regular_Expression_Matching.py
@@ -1,22 +1,30 @@
-class Solution:
-    def isMatch(self, s: str, p: str) -> bool:
-        memo = {}  
+class Solution(object):
+    def isMatch(self, s, p):
+  
+        cache = {}
+        def dfs(i, j):
+            if (i,j) in cache:
+                return cache[(i, j)]
+            
+            if i >= len(s) and j >= len(p):
+                return True
+            
+            if j >= len(p):
+                return False
+            
+            match = i < len(s) and (s[i] == p[j] or p[j] == ".")
 
-        def dp(i, j):
-            if (i, j) in memo:
-                return memo[(i, j)]
+            if (j+1) < len(p) and p[j+1] == "*":
+                cache[(i, j)] = dfs(i, j+2) or (match and dfs(i+1, j))
+                
+                return cache[(i, j)]
+            
+            if match:
+                cache[(i,j)] = dfs(i+1, j+1)
+                return cache[(i, j)]
+            
+            cache [(i, j)] = False
+            return False
 
-            if j == len(p):  
-                return i == len(s) 
-
-            first_match = i < len(s) and (p[j] == s[i] or p[j] == '.')
-
-            if j + 1 < len(p) and p[j + 1] == '*':
-               
-                memo[(i, j)] = dp(i, j + 2) or (first_match and dp(i + 1, j))
-            else:
-                memo[(i, j)] = first_match and dp(i + 1, j + 1)
-
-            return memo[(i, j)]
-
-        return dp(0, 0)
+        return dfs(0, 0)
+        
