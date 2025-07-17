aae041b7fd26481bf1b6134d8c7f687ebdc887c9
Akshith KL <160204272+akshithkl@users.noreply.github.com>
2025-07-17 16:32:30 +0530

    Create 1980. Find Unique Binary String_1.py

---
+++ Medium/1980. Find Unique Binary String_1.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def findDifferentBinaryString(self, nums):
+        n = len(nums)
+        res = []
+
+        for i in range(n):
+            # Flip the i-th bit of the i-th string
+            if nums[i][i] == '0':
+                res.append('1')
+            else:
+                res.append('0')
+
+        return ''.join(res)
+
+
+# | Aspect               | Value             |
+# | -------------------- | ----------------- |
+# | **Algorithm**        | Greedy / Diagonal |
+# | **Approach**         | Cantor's diagonal |
+# | **Time Complexity**  | `O(n)`            |
+# | **Space Complexity** | `O(n)`            |
