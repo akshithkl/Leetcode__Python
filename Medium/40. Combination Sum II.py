19efe1491a5a05215b7434bd14787f12d3749b2c
Akshith KL <160204272+akshithkl@users.noreply.github.com>
2025-08-06 17:37:06 +0530

    Create 40. Combination Sum II.py

---
+++ Medium/40. Combination Sum II.py
@@ -0,0 +1,29 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+
+        res = []
+        def backtrack(cur, pos, target):
+            if target == 0:
+                res.append(cur.copy())
+            if target <= 0:
+                return
+            
+            prev = -1
+            for i in range(pos, len(candidates)):
+                if candidates[i] == prev:
+                    continue
+                cur.append(candidates[i])
+                backtrack(cur, i+1, target - candidates[i])
+                cur.pop()
+                prev = candidates[i]
+        
+        backtrack([], 0, target)
+        return res
+
+# | Category             | Value                              |
+# | -------------------- | ---------------------------------- |
+# | **Algorithm**        | Backtracking with pruning          |
+# | **Technique**        | Sort + Skip Duplicates + Backtrack |
+# | **Time Complexity**  | O(2ⁿ) worst-case, O(k × m) average |
+# | **Space Complexity** | O(n) call stack, O(k × m) output   |
